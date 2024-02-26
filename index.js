ac5d6ba04757e391972f4c55501d4e2b91420ec4
yangdong <27282480@qq.com>
2024-02-26 15:33:39 +0800

    .

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 
 const drawWorldArmadillo = require('./draw/draw-armadillo');
-const helloWorldBanana = require('./draw/draw-banana')
+const helloWorldBanana = require('./draw/draw-banana');
+const helloWorldBees = require('./draw/draw-bees');
+
+// const helloWorldBanana = require('draw-banana')
 
 function run() {
     drawWorldArmadillo();
     helloWorldBanana();
+    helloWorldBees();
 }  
 
 run()
